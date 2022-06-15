e01a666b57e143742d9b2a1963cdddc8536c54c6
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:56:05 -0300

    Add files via upload

---
+++ 45-NumeroCorrespondienteADiasDeSemana/45-NumeroCorrespondienteADiasDeSemana/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _45_NumeroCorrespondienteADiasDeSemana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Ingrese un numero de 1 a 7 para mostrar el nombre del dia o ingrese 0 para cerrar:");
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+
+            while (a < 0 || a > 7)
+            {
+                Console.WriteLine("Valor fuera de rango, re-ingrese:");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            while (a != 0)
+            {
+                
+
+                if (a == 1)
+                {
+                    Console.WriteLine("Es lunes");
+                }
+                
+                if (a == 2)
+                {
+                    Console.WriteLine("Es martes");
+                }
+                
+                if (a == 3)
+                {
+                    Console.WriteLine("Es Miercoles");
+                }
+                
+                if (a == 4)
+                {
+                    Console.WriteLine("Es jueves");
+                }
+                
+                if (a == 5)
+                {
+                    Console.WriteLine("Es viernes");
+                }
+                
+                if (a == 6)
+                {
+                    Console.WriteLine("Es sabado");
+                }
+                
+                if (a == 7)
+                {
+                    Console.WriteLine("Es domingo");
+                }
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            
+
+            Console.ReadKey();
+           
+        }
+    }
+}
